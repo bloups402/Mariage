1d4fb3f815c3e8d3bcd891671ded44b4f9db3bb8
bloups402 <antonin.runel@gmail.com>
2026-05-27 11:04:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ document.getElementById("rsvpForm").addEventListener("submit", function(event) {
     event.preventDefault();
     document.getElementById("message").textContent = "Merci pour votre réponse ❤️";
 });
+``
