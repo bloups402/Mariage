e09a8a35824c08ee2cd03c5fca8401a81673c12c
bloups402 <antonin.runel@gmail.com>
2026-05-27 10:44:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("rsvpForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("message").textContent = "Merci pour votre réponse ❤️";
+});
